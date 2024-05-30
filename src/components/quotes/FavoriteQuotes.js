77e91220489cf90fceb3b832a40b71343f9a7dc1
Shanna <shanna.r.walsh@gmail.com>
2024-05-30 15:47:58 -0500

    favorites conditional text updated for clarity

--- src/components/quotes/FavoriteQuotes.js
+++ src/components/quotes/FavoriteQuotes.js
@@ -8,19 +8,19 @@ const FavoriteQuotes = ({ favoriteQuotes,maxFaves, removeFromFavorites }) => {
     return (
         <section className="favorite-quotes">
           <div className="wrapper quotes">
-          <div className="favorite-quotes-description">
-              <p>You can add up to {remainingFavoriteAmount} more {remainingFavoriteAmount === 1 ? "quote" : "quotes"} to your top three favorites by selecting from the options below. 
+            <div className="favorite-quotes-description">
+              <p>You can add {remainingFavoriteAmount} {remainingFavoriteAmount < 3 && "more"} {remainingFavoriteAmount === 1 ? "quote" : "quotes"} to your top three favorites by selecting from the options below. 
               <br/>
               Once you choose your favories, they will appear here:</p>
             </div>
-            <h3>Top 3 favorite quotes</h3>
+            {remainingFavoriteAmount < 3 && <h3>Top 3 favorite quotes</h3>}
             {favoriteQuotes.length > 0 && 
-            <ul>{favoriteQuotes.map((quote, index) => (
-                <FavoriteQuoteCard
-                key={quote.id}
-                listPosition={index + 1}
-                quote={quote}
-                removeFromFavorites={removeFromFavorites}/>
+               <ul>{favoriteQuotes.map((quote, index) => (
+                  <FavoriteQuoteCard
+                  key={quote.id}
+                  listPosition={index + 1}
+                  quote={quote}
+                  removeFromFavorites={removeFromFavorites}/>
             ))}</ul>}
           </div>
         </section>
